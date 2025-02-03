9d3bff03c56767d1642ee226ffe9a745810d3235
Medivash <lucko01@bk.ru>
2025-02-03 15:15:40 +0300

    Обновление_теста

--- src/test/java/ElectTest.java
+++ src/test/java/ElectTest.java
@@ -1,34 +1,31 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class ElectTest {
+public class ElectTest extends BaseTest {
+
+    protected WebDriver driver;
     @Test
     public void testCoggleTitle() {
-        // Установка пути к chromedriver
-        System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
+        driver.get("https://www.google.com");
+        System.out.println("Заголовок страницы: " + driver.getTitle());
+    }
 
-        // Настройка опций Chrome
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*"); // Разрешить все origins
-        options.addArguments("--no-sandbox"); // Отключить sandbox
-        options.addArguments("--disable-dev-shm-usage"); // Отключить использование /dev/shm
+        driver = new ChromeDriver(options);
+    }
 
-        // Создание экземпляра WebDriver с опциями
-        WebDriver driver = new ChromeDriver(options);
-
-        // Открытие страницы Google
-        driver.get("https://www.google.com");
-
-        // Вывод заголовка страницы
-        System.out.println("Заголовок страницы: " + driver.getTitle());
-
-        // Закрытие браузера
-        driver.quit();
-
-        System.out.println("New_test");
-        System.out.println("New_test");
-        System.out.println("New_test");
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
