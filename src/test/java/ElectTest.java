9d76892cc90b938780fead8cedd3915b2a50ad82
Medivash <lucko01@bk.ru>
2025-02-03 15:20:26 +0300

    Проверка git 3

--- src/test/java/ElectTest.java
+++ src/test/java/ElectTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class ElectTest extends BaseTest {
+public class ElectTest{
 
     protected WebDriver driver;
     @Test
