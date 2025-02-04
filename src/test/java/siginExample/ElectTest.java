c2b8ef0c8e92d9e88f5300f59d876c0728b50c08
Medivash <lucko01@bk.ru>
2025-02-04 11:37:03 +0300

    Проверка git 3

--- src/test/java/siginExample/ElectTest.java
+++ src/test/java/siginExample/ElectTest.java
@@ -1,17 +1,23 @@
+package siginExample;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.FindBy;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class ElectTest{
+    @FindBy(xpath = " /html/body/div[1]/div[3]/header/div/div[1]/div[2]/a")
+    private WebElement siginBtn;
 
     protected WebDriver driver;
     @Test
     public void testCoggleTitle() {
-        driver.get("https://www.google.com");
-        System.out.println("Заголовок страницы: " + driver.getTitle());
+        driver.get("https://github.com/");
+        System.out.println("Найдена страница: " + driver.getTitle());
     }
 
     @BeforeMethod
@@ -20,6 +26,7 @@ public class ElectTest{
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*"); // Разрешить все origins
         driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
     }
 
     @AfterMethod
