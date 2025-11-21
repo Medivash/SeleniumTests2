88ec78fc8abfde520d2d3d1eaf0d4b70f8050db8
Medivash <lucko01@bk.ru>
2025-11-21 11:20:59 +0300

    2-nd selenium AT. githublogin

--- src/test/java/PetSeleniumTests/PageFind/PageFindTest2.java
+++ src/test/java/PetSeleniumTests/PageFind/PageFindTest2.java
@@ -1,7 +1,15 @@
 package PetSeleniumTests.PageFind;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -9,10 +17,27 @@ import org.testng.annotations.Test;
 public class PageFindTest2 {
 
     protected WebDriver driver;
+
     @Test
     public void testCoggleTitle() {
         driver.get("https://github.com/");
         System.out.println("Найдена страница: " + driver.getTitle());
+    }
+
+    @Test
+    public void testGithubLogin() {
+        driver.get("https://github.com/login");
+
+        driver.findElement(By.id("login_field")).sendKeys("lucko01@bk.ru");
+        driver.findElement(By.id("password")).sendKeys("dima2001D");
+        driver.findElement(By.name("commit")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        WebElement otpInput = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.id("otp"))
+        );
+
+        Assert.assertTrue(otpInput.isDisplayed(), "Страница подтверждения с кодом не появилась.");
     }
 
     @BeforeMethod
